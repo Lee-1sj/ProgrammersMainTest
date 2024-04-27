3021ace1c7d100e62592107875fc64239ce9b1e4
Lee-1sj <sukjin0112@naver.com>
2024-04-27 16:13:04 +0900

    [create] 프로그래머스 메인화면 연습

---
+++ js/programmers.js
@@ -0,0 +1,7 @@
+function showLogin() {
+    window.open("login.html", "로그인", "width=550, height=400, top=100, left=100");
+}
+
+function showRegister() {
+    window.open("member-register.html", "회원가입", "width=1500, height=3000, top=100, left=100");
+}
