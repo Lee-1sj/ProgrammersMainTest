7494258beadcea8707dac3763b7bd4f949b9ebce
Lee-1sj <sukjin0112@naver.com>
2024-04-30 21:00:58 +0900

    [update] 회전목마 부분 update

--- js/programmers.js
+++ js/programmers.js
@@ -27,3 +27,89 @@ document.addEventListener("DOMContentLoaded", function () {
         Header.style.transition = "height 0.3s ease";
     });
 });
+
+function call_js() {
+    let slideshow = document.querySelector(".slideshow");
+    let slideshow_slides = document.querySelector(".slideshow_slides");
+
+    let aSlidesArray = document.querySelectorAll(".slideshow_slides a");
+    let prev = document.querySelector(".prev");
+    let next = document.querySelector(".next");
+    let indicators = document.querySelectorAll(".indicator a");
+
+    let currentIndex = 0;
+    let timer = 3000;
+    let slideCount = aSlidesArray.length;
+
+    for (let i = 0; i < slideCount; i++) {
+        let newLeft = `${i * 100}%`;
+        aSlidesArray[i].style.left = newLeft;
+    }
+
+    function gotoSlide(index) {
+        currentIndex = index;
+        let newLeft = index * -100 + '%';
+        slideshow_slides.style.left = newLeft;
+
+        for (let i = 0; i < indicators.length; i++) {
+            indicators[i].classList.remove("active")
+        }
+        indicators[currentIndex].classList.add("active");
+    }
+    gotoSlide(0);
+
+    function startTimer() {
+        timer = setInterval(function () {
+            currentIndex += 1;
+            let index = currentIndex % slideCount;
+            gotoSlide(index);
+        }, 3000);
+    }
+    startTimer();
+
+    slideshow_slides.addEventListener("mouseenter", function () {
+        clearInterval(timer);
+    });
+
+    slideshow_slides.addEventListener("mouseleave", function () {
+        startTimer();
+    });
+
+    prev.addEventListener("mouseenter", function () {
+        clearInterval(timer);
+    });
+
+    next.addEventListener("mouseenter", function () {
+        clearInterval(timer);
+    });
+
+    prev.addEventListener("click", function (e) {
+        e.preventDefault() // a tag 기본기능 막음
+        currentIndex -= 1;
+        if (currentIndex < 0) {
+            currentIndex = slideCount - 1;
+        }
+        gotoSlide(currentIndex);
+    });
+
+    next.addEventListener("click", function (e) {
+        e.preventDefault() // a tag 기본기능 막음
+        currentIndex += 1;
+        if (currentIndex > slideCount - 1) {
+            currentIndex = 0;
+        }
+        gotoSlide(currentIndex);
+    });
+
+    indicators.forEach((e) => {
+        e.addEventListener("mouseenter", () => {
+            clearInterval(timer);
+        });
+    });
+    for (let i = 0; i < indicators.length; i++) {
+        indicators[i].addEventListener("click", (e) => {
+            e.preventDefault();
+            gotoSlide(i);
+        });
+    }
+} 
